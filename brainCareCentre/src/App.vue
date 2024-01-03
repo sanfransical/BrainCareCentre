e8e23152aefeb17cda740c5cbe2d0c5fdef6fd0f
StepthenFriedman <19902512605@163.com>
2024-01-03 08:53:32 +0800

    1/3

--- brainCareCentre/src/App.vue
+++ brainCareCentre/src/App.vue
@@ -6,7 +6,7 @@ import TheWelcome from './components/TheWelcome.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <img src="../img/csu.jpg"/>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
